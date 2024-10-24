c1d55cc4225061b7acaad864050cf551edd48c76
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-23 20:44:47 -0600

    Add FAQ, Footer, and Testimonials sections to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Benefits from "./sections/Benefits";
+import { Benefits } from "./sections/Benefits";
+import { FAQ } from "./sections/Faq";
+import { Footer } from "./sections/Footer";
 import { Hero } from "./sections/Hero";
+import { Testimonials } from "./sections/Testimonials";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background transition-colors duration-300">
       <Hero />
       <Benefits />
+      <Testimonials />
+      <FAQ />
+      <Footer />
     </div>
   );
 }
