0c939554b4bc6c28b5e4e92b7890f7fa3aae7127
up0808 <upboard08082000@gmail.com>
2025-10-26 07:51:51 +0530

    Update Hero.tsx

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link"
 import { Button } from "@/components/ui/button";
 import type { AnimatedProps } from "@/types/motion";
 import { motion } from "framer-motion";
@@ -106,13 +107,12 @@ export function Hero({ id }: AnimatedProps) {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-           <Button
-  href="https://chat.search.fbadevishant.qzz.io"
-  className="bg-gradient-to-r from-cyan-600 via-purple-600 to-blue-600 hover:from-cyan-700 hover:via-purple-700 hover:to-blue-700 text-white px-8 py-6 text-lg rounded-full shadow-lg hover:shadow-xl transition-all duration-300 flex items-center"
->
-  <Search className="w-5 h-5 mr-2" />
-  Start Searching Now
-</Button>
+            <Link href="https://chat.search.fbadevishant.qzz.io" target="_blank">
+      <Button className="bg-gradient-to-r from-cyan-600 via-purple-600 to-blue-600 hover:from-cyan-700 hover:via-purple-700 hover:to-blue-700 text-white px-8 py-6 text-lg rounded-full shadow-lg hover:shadow-xl transition-all duration-300 flex items-center">
+        <Search className="w-5 h-5 mr-2" />
+        Start Searching Now
+      </Button>
+    </Link>
           </motion.div>
 
           <motion.div
